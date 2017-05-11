f4e70129d7100db76a8549cce87197743e5eeaa7
Jose Maria <jotaememoreno@gmail.com>
2017-05-11 22:35:21 +0200

    Se añaden formularios adicionales para la creacion de recetas

---
+++ src/AppBundle/Form/TagsType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Entity\Tags;
+
+class TagsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('tag');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' =>Tags::class,
+
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_tags';
+    }
+
+
+}
