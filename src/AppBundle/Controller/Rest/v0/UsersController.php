6f3946f537dda9312ca5b541a3b0141f2e5f32e8
Jose Maria <jotaememoreno@gmail.com>
2017-05-25 21:14:08 +0200

    Se implementa el login que devuelve el token del usuario

--- src/AppBundle/Controller/Rest/v0/UsersController.php
+++ src/AppBundle/Controller/Rest/v0/UsersController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller\Rest\v0;
 
 use AppBundle\Entity\User;
+use AppBundle\Form\UserLoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -12,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Form\UserType;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class UsersController extends Controller
 {
@@ -22,11 +24,18 @@ class UsersController extends Controller
      */
     public function createUserAction(Request $request)
     {
+        // TODO: Check if user already exists in db
         $em = $this->getDoctrine()->getManager();
         $user = new User();
         $form = $this->get('form.factory')->createNamed('user_form', UserType::class, $user);
         $form->handleRequest($request);
         if ($form->isValid() && $form->isSubmitted()) {
+            $user_exists = $em->getRepository('AppBundle:User')->findOneBy(array(
+                'email'=>$form->getData()->getEmail()
+            ));
+            if($user_exists){
+                return new Response(\GuzzleHttp\json_encode('User already exists'), Response::HTTP_ACCEPTED);
+            }
             $encoder = $this->container->get('security.password_encoder');
             $encoded = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($encoded);
@@ -40,6 +49,39 @@ class UsersController extends Controller
             return new JsonResponse(['token' => $token]);
         }
         return $form;
+    }
+
+    /**
+     * @Rest\Post("/users/auth")
+     */
+    public function loginUserAction(Request $request)
+    {
+        $form = $this->get('form.factory')->createNamed('login_form', UserLoginType::class);
+        $form->handleRequest($request);
+        $email = $form->getData()->getEmail();
+        $password = $form->getData()->getPassword();
+        if(isset($email) && isset($password)){
+            $em = $this->getDoctrine()->getManager();
+            $encoder = $this->container->get('security.password_encoder');
+            $existing_user = $em->getRepository('AppBundle:User')->findOneBy(array(
+                'email' => $email
+            ));
+            if($existing_user){
+                if($encoder->isPasswordValid($existing_user,$password)){
+                    $token = $this->get('lexik_jwt_authentication.encoder')
+                        ->encode([
+                            'username' => $existing_user->getEmail(),
+                            'exp' => time() + 36000000
+                        ]);
+                    return new JsonResponse(['token' => $token]);
+                }
+            }else{
+                return new Response(\GuzzleHttp\json_encode('Resource not found.'), Response::HTTP_NOT_FOUND);
+            }
+        }
+        return new Response(\GuzzleHttp\json_encode('Resource not found.'), Response::HTTP_NOT_FOUND);
+
+
     }
 
 }
